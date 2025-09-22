6ff21a6e854331db464d08fc950e570fef3ce979
codebubo <codebubo@gmail.com>
2025-09-22 20:26:23 +0200

    file update

--- font-size/_extensions/font-size/font-size.lua
+++
@@ -1,9 +0,0 @@
-function Div(el)
-  -- Kontrollera om div har klassen 'font-size-0.8'
-  if el.classes:includes('font-size-0.8') then
-    -- Lägg till inline CSS för fontstorlek
-    el.attributes['style'] = 'font-size: 0.8em;'
-    return el
-  end
-  return el
-end
